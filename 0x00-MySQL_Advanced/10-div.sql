3d026d638b28d06659484b2f665c81b54a0899d4
mouad mennioui <moad.menniui@gmail.com>
2024-04-25 18:22:46 +0100

    Create 10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- Script to create the SafeDiv function
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10,4)
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
