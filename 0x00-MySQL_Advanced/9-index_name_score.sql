a298712b553473cc5f086fa69333fb564b8157ad
mouad mennioui <moad.menniui@gmail.com>
2024-04-25 18:23:38 +0100

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,6 +1,2 @@
 -- Script to create an index on the first letter of 'name' and 'score'
-DELIMITER $$
-
-CREATE INDEX idx_name_first_score ON names ((LEFT(name, 1)), score)$$
-
-DELIMITER ;
+CREATE INDEX idx_name_first_score ON names(name(1), score);
